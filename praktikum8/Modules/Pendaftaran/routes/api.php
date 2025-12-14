61492f3722d480dc6edfa236dba74fbd0d62d155
salwaazkiamulyana5-svg <salwaazkiamulyana5@gmail.com>
2025-12-15 01:46:07 +0700

    Delete praktikum8 directory

--- praktikum8/Modules/Pendaftaran/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Pendaftaran\Http\Controllers\PendaftaranController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('pendaftarans', PendaftaranController::class)->names('pendaftaran');
-});
